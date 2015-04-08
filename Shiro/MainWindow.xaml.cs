411b59efc2f7ba52a6f6de5e5375a044eeb6b464
fvidalfacal <florianvidalfacal@gmail.com>
2015-04-08 16:12:02 +0200

    * Update MainWindow

--- Shiro/MainWindow.xaml.cs
+++ Shiro/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -24,6 +25,44 @@ namespace Shiro
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void userConnection_Click(object sender, RoutedEventArgs e)
+        {
+            string userLogin = login.Text;
+            //string userPassword = SHA1(password.Password);
+            string userPasswordEncrypted = GetSHA1HashData(password.Password);
+            //var command = Class.Connection.Command(String.Format("SELECT login, password WHERE login = {0} AND password = {1}", userLogin , password.Password));
+            MessageBox.Show(login.Text);
+            MessageBox.Show(userPasswordEncrypted);
+        }
+
+
+        /// <summary>
+        /// take any string and encrypt it using SHA1 then
+        /// return the encrypted data
+        /// </summary>
+        /// <param name="data">input text you will enterd to encrypt it</param>
+        /// <returns>return the encrypted text as hexadecimal string</returns>
+        private string GetSHA1HashData(string data)
+        {
+            //create new instance of md5
+            SHA1 sha1 = SHA1.Create();
+
+            //convert the input text to array of bytes
+            byte[] hashData = sha1.ComputeHash(Encoding.Default.GetBytes(data));
+
+            //create new instance of StringBuilder to save hashed data
+            StringBuilder returnValue = new StringBuilder();
+
+            //loop for each byte and add it to StringBuilder
+            for (int i = 0; i < hashData.Length; i++)
+            {
+                returnValue.Append(hashData[i].ToString());
+            }
+
+            // return hexadecimal string
+            return returnValue.ToString();
         }
     }
 }
